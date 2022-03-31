5690881a3051d80cf4c3b7b2bf0c2d11d088a27e
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-04-01 00:25:35 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/lato';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
