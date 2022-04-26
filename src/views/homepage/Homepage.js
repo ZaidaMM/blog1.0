e21822589fdac9f06984db5013fb02b84a8d31e0
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-04-26 01:58:33 +0100

    Build commit

--- src/views/homepage/Homepage.js
+++ src/views/homepage/Homepage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Hero from './components/Hero';
 import BlogList from './components/BlogList';
-// import { articles } from '../../shared/articles';
 
 function Homepage() {
   return (
